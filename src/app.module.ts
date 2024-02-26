01b84c08dbe63bae2b7d3884b442fa3ee6633660
miladifarah <miladifarah825@gmail.com>
2024-02-26 22:16:13 +0100

    typeormv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, Session } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -9,7 +9,7 @@ import { Invoice } from './entity/invoice';
 import { Message } from './entity/message';
 import { Payment } from './entity/payment';
 import { Subplan } from './entity/subplan';
-import { Subscription } from 'rxjs';
+import { Subscription } from './entity/subscription';
 import { Sessions } from './entity/sessions';
 
 
@@ -18,15 +18,14 @@ import { Sessions } from './entity/sessions';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        type: 'postgres' , // Replace with your database type (e.g., 'mysql')
+        type: 'postgres' , 
         host: configService.get('DATABASE_HOST'), 
         port: configService.get('DATABASE_PORT'), 
         username: configService.get('DATABASE_USER'),
         password: configService.get('DATABASE_PASSWORD'),
-        database: configService.get('DATABASE_NAME'), // Replace with your database name
-        entities: [User,Calendar,Invoice,Message,Payment,Sessions
-          ,Subplan,Subscription], // List your entities here
-        synchronize: true, // Set to false in production for safety
+        database: configService.get('DATABASE_NAME'), 
+        entities: [User,Calendar,Invoice,Message,Payment,Sessions,Subplan,Subscription], 
+        synchronize: true, 
       }),
       inject: [ConfigService],
     }),],
--- src/entity/invoice.ts
+++ src/entity/invoice.ts
@@ -1,7 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
-export class Invoice{
+export class Invoice { 
   @PrimaryGeneratedColumn()
   id: number;
 
